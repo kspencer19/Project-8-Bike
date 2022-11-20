94dd9882444e8bbbba926b31c18f4d3d9e630292
kspencer19 <54643900+kspencer19@users.noreply.github.com>
2022-11-19 21:39:33 -0700

    Update bike.cpp

--- bike.cpp
+++ bike.cpp
@@ -386,6 +386,7 @@ void grass(void)
    glPushMatrix();
    glEnable(GL_TEXTURE_2D);
    glBindTexture(GL_TEXTURE_2D, texture);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
    glBegin(GL_QUADS);
 
    glTexCoord2f(0, 0);
